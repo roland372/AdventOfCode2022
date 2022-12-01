fca4b17caecd2211cceacaed7fd7b9ca3618eb1e
roland372 <damfat94@gmail.com>
2022-12-01 23:29:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+var fileContent = fs
+	.readFileSync('file.txt', 'utf8')
+	.replace(/\r/g, '')
+	.split('\n\n');
+
+function part1() {
+	let maxCalories = fileContent.map(el => {
+		return el.split('\n').reduce((prev, curr) => prev + curr * 1, 0);
+	});
+	return Math.max(...maxCalories);
+}
+
+function part2() {
+	let maxCalories = fileContent.map(el => {
+		return el.split('\n').reduce((prev, curr) => prev + curr * 1, 0);
+	});
+	return maxCalories
+		.sort((a, b) => b - a)
+		.slice(0, 3)
+		.reduce((prev, curr) => prev + curr, 0);
+}
+
+console.log(part1());
+console.log(part2());
